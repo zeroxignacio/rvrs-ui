08b6f08c2b0906d3b3c840c6743cc83b1f18d604
0xKillua <zeroxkillua@gmail.com>
2022-03-27 21:35:35 +0100

    update: nav animations and transitions

--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -1,21 +1,15 @@
 import React, { useState } from 'react'
 import styled, { keyframes } from 'styled-components'
 import { useWallet } from '@binance-chain/bsc-use-wallet'
-import { usePriceCakeBusd } from 'state/hooks'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, ButtonGroup, Button } from 'react-bootstrap'
-import { getBalanceNumber } from 'utils/formatBalance'
-import useTokenBalance from 'hooks/useTokenBalance'
 import useWalletModal from 'components/WalletModal'
-import { getCakeAddress } from 'utils/addressHelpers'
 import { NavLink } from 'react-router-dom'
 import rvrs from 'config/constants/rvrs'
 import { Flex } from '../layout/flex'
 
 const Nav = (props) => {
   const { account, connect, reset } = useWallet()
-  const rvrsPriceUsd = usePriceCakeBusd()
-  const rvrsBalance = getBalanceNumber(useTokenBalance(getCakeAddress())).toLocaleString('en-us', { maximumFractionDigits: 0 });
   const { onPresentConnectModal } = useWalletModal(connect, reset)
 
   return (
@@ -51,7 +45,7 @@ const Nav = (props) => {
       </ButtonGroup>
       <ButtonGroup>
         {account != null && account.length > 1 ?
-          <ConnectButton
+          <ConnectedButton
             style={{ justifyContent: "space-between" }}
             as={StyledNavLink}
             to="/dashboard"
@@ -63,7 +57,7 @@ const Nav = (props) => {
               <object type="image/svg+xml" data="/images/hmny.svg" width="50px">&nbsp;</object>
               <div style={{ marginLeft: '10px', marginRight: '20px' }}>{account.substring(0, 6)} </div>
             </Flex>
-          </ConnectButton>
+          </ConnectedButton>
           :
           <ConnectButton
             as={StyledNavLink}
@@ -93,37 +87,37 @@ const MenuContainer = styled(Container)`
 `
 
 const StyledButton = styled.div`
+  background-color: #2D3544;
+  font-size: 18px;
+  font-weight: 500;
   text-align: center;
   border: #FFFF solid 0px;
   border-radius: 35px;
-  background-color: #2D3544;
   padding-top: 20px;
   padding-bottom: 20px;
-  padding-left: 28px;
-  padding-right: 28px;
-  font-size: 18px;
-  font-weight: 500;
+  padding-left: 27px;
+  padding-right: 27px;
   transition: 0.3s ease-in-out;
-
   &:hover  {
-    background-color: #363F50;
+    background-color: #374052;
   }
 `
 
-const ConnectButton = styled.div`
+const ConnectedButton = styled.div`
+  background-color: #2D3544;
+  font-size: 18px;
+  font-weight: 500;
   text-align: center;
   border: 1.5px;
   border-style: solid !important;
   border-color: #CBCBCB !important;
   border-radius: 35px;
-  background-color: #2D3544;
   padding: 5px;
-  font-size: 18px;
-  font-weight: 500;
   transition: 0.3s ease-in-out;
-
   &:hover  {
-    background-color: #363F50;
+    background-color: #374052;
+    box-shadow: 20px 0px 40px -20px #55747D, -20px 0px 20px -20px #4B5674;
+    border-color: #FFFF !important;
     transform: translate(-6px)
   }
 `
@@ -140,6 +134,27 @@ const pulse = keyframes`
   }
 `
 
+const ConnectButton = styled.div`
+  background-color: #2D3544;
+  font-size: 18px;
+  font-weight: 500;
+  text-align: center;
+  border: 1.5px;
+  border-style: solid !important;
+  border-color: #CBCBCB !important;
+  border-radius: 35px;
+  padding: 5px;
+  transition: 0.3s ease-in-out;
+  box-shadow: -30px 0px 50px -5px #55747D, 30px 0px 50px -5px #4B5674;
+  &:hover  {
+    font-weight: 700;
+    border-color: #FFFF !important;
+    box-shadow: 20px 0px 40px 0px #55747D, -20px 0px 40px 0px #4B5674;
+    background-color: #374052;
+    transform: translate(-6px)
+  }
+`
+
 const ButtonContainer = styled.div`
   border-radius: 35px;
   background-color: #2D3544;
@@ -149,6 +164,10 @@ const ButtonContainer = styled.div`
   border-color: #CBCBCB !important;
   border-style: solid !important;
   animation: ${pulse} 5s infinite ease-out;
+  transition: 0.3s ease-in-out;
+  &:hover  {
+    border-color: #FFFF !important;
+  }
 `
 
 const activeClassName = 'ACTIVE'
