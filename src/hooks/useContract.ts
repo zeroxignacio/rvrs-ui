5addcc79471b14c4a59203adb70cea824349ba4e
0xKillua <zeroxkillua@gmail.com>
2022-03-28 12:52:59 +0100

    cleanup: formatting and files removal
    could break some stuff. pushing to testing and then merging to develop after deployment

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -5,26 +5,14 @@ import useWeb3 from 'hooks/useWeb3'
 import {
   getMasterChefAddress,
   getCakeAddress,
-  getLotteryAddress,
-  getLotteryTicketAddress,
-  getWheelAddress,
   getWbnbAddress,
-  getWheel2Address,
   getSousChefAddress,
   getAutoRvrsAddress,
   getAirdropAddress
 } from 'utils/addressHelpers'
-import { pools2Config, poolsConfig } from 'config/constants'
-import { PoolCategory } from 'config/constants/types'
-import ifo from 'config/abi/ifo.json'
+import { pools2Config } from 'config/constants'
 import erc20 from 'config/abi/erc20.json'
-import rabbitmintingfarm from 'config/abi/rabbitmintingfarm.json'
 import devFeeProcessor from 'config/abi/devFeeProcessor.json'
-import nftSale from 'config/abi/nftSale.json'
-import pancakeRabbits from 'config/abi/pancakeRabbits.json'
-import lottery from 'config/abi/lottery.json'
-import wheel from 'config/abi/wheel.json'
-import lotteryTicket from 'config/abi/lotteryNft.json'
 import masterChef from 'config/abi/masterchef.json'
 import sousChef from 'config/abi/sousChef.json'
 import autoRvrs from 'config/abi/autorvrs.json'
@@ -43,15 +31,6 @@ const useContract = (abi: AbiItem, address: string, contractOptions?: ContractOp
   return contract
 }
 
-/**
- * Helper hooks to get specific contracts (by ABI)
- */
-
-export const useIfoContract = (address: string) => {
-  const ifoAbi = (ifo as unknown) as AbiItem
-  return useContract(ifoAbi, address)
-}
-
 export const useERC20 = (address: string) => {
   const erc20Abi = (erc20 as unknown) as AbiItem
   return useContract(erc20Abi, address)
@@ -65,44 +44,9 @@ export const useWone = () => {
   return useERC20(getWbnbAddress())
 }
 
-export const useRabbitMintingFarm = (address: string) => {
-  const rabbitMintingFarmAbi = (rabbitmintingfarm as unknown) as AbiItem
-  return useContract(rabbitMintingFarmAbi, address)
-}
-
 export const useDevFeeProcessorContract = (address: string) => {
   const rabbitMintingFarmAbi = (devFeeProcessor as unknown) as AbiItem
   return useContract(rabbitMintingFarmAbi, address)
-}
-
-export const useNftSaleContract = (address: string) => {
-  const nftSaleAbi = (nftSale as unknown) as AbiItem
-  return useContract(nftSaleAbi, address)
-}
-
-export const usePancakeRabbits = (address: string) => {
-  const pancakeRabbitsAbi = (pancakeRabbits as unknown) as AbiItem
-  return useContract(pancakeRabbitsAbi, address)
-}
-
-export const useLottery = () => {
-  const abi = (lottery as unknown) as AbiItem
-  return useContract(abi, getLotteryAddress())
-}
-
-export const useMoneyWheel = () => {
-  const abi = (wheel as unknown) as AbiItem
-  return useContract(abi, getWheelAddress())
-}
-
-export const useMoneyWheel2 = () => {
-  const abi = (wheel as unknown) as AbiItem
-  return useContract(abi, getWheel2Address())
-}
-
-export const useLotteryTicket = () => {
-  const abi = (lotteryTicket as unknown) as AbiItem
-  return useContract(abi, getLotteryTicketAddress())
 }
 
 export const useMasterchef = () => {
