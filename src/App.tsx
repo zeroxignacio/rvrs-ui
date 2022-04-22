1ba3ed7f62cd600a175e54f4af9668b23cabd958
0xKillua <zeroxkillua@gmail.com>
2022-04-22 15:25:29 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
         <Suspense fallback>
           <Switch>
             <Route path="/" exact>
-              <LANDING />
+              <DASHBOARD />
             </Route>
             <Route path="/bonds">
               <BONDS />
