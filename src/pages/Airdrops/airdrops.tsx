219eca54e58e7a130888353dc3c6c78613c86562
0xKillua <zeroxkillua@gmail.com>
2022-04-22 17:57:46 +0400

    box-shadow to containers

--- src/pages/Airdrops/airdrops.tsx
+++ src/pages/Airdrops/airdrops.tsx
@@ -181,6 +181,11 @@ const Wrap = styled(Container)`
   border-width: 1px;
   border-color: #313131;
   border-style: solid;
+  box-shadow: 5px 5px 30px -22px #55747d;
+  transition: all 0.3s ease-in-out;
+  :hover {
+      box-shadow: 5px 5px 30px -18px #55747d;
+  }
 `
 
 export default Airdrop
