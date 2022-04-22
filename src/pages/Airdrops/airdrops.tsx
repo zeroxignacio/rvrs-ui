df29c70cbff5515636b6609223442b03a49000ba
0xKillua <zeroxkillua@gmail.com>
2022-04-22 18:05:41 +0400

    removed hover animation on box shadow

--- src/pages/Airdrops/airdrops.tsx
+++ src/pages/Airdrops/airdrops.tsx
@@ -175,17 +175,14 @@ const Airdrop = () => {
   )
 }
 
+
 const Wrap = styled(Container)`
-  border-radius: 5px;
+  border-radius: 7px;
   padding: 2px;
   border-width: 1px;
   border-color: #313131;
   border-style: solid;
-  box-shadow: 5px 5px 30px -22px #55747d;
-  transition: all 0.3s ease-in-out;
-  :hover {
-      box-shadow: 5px 5px 30px -18px #55747d;
-  }
-`
+  box-shadow: 5px 5px 28px -20px #55747d;
+  `
 
 export default Airdrop
