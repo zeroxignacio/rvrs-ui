80897313bd004b0306c53f456a12df1cfff75b33
FoxTheFarmer <FoxTheFarmer@cryptina.info>
2022-04-28 15:28:36 -0600

    adding data pulls and function calls for veRVRS

--- src/state/stake/fetchPools.ts
+++ src/state/stake/fetchPools.ts
@@ -1,7 +1,8 @@
 import masterchefAbi from 'config/abi/masterchef.json';
 import autorvrsAbi from 'config/abi/autorvrs.json';
 import multicall from 'utils/multicall';
-import {getAutoRvrsAddress, getMasterChefAddress} from 'utils/addressHelpers';
+import { getAutoRvrsAddress, getMasterChefAddress, getVeRvrsAddress } from 'utils/addressHelpers';
+import veRrvsAbi from "config/abi/veRvrs.json";
 
 // eslint-disable-next-line import/prefer-default-export
 export const fetchPoolsTotalStaking = async () => {
@@ -24,4 +25,44 @@ export const fetchPoolsTotalStaking = async () => {
   const pricePerFullShare = await multicall(autorvrsAbi, calls2);
 
   return { userInfo, pricePerFullShare };
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const fetchPoolsVeRvrsData = async () => {
+  const veRvrsAddress = getVeRvrsAddress()
+  const calls = [
+    {
+      address: veRvrsAddress,
+      name: 'totalStaked',
+      params: []
+    },
+    {
+      address: veRvrsAddress,
+      name: 'totalSupply',
+      params: []
+    },
+    {
+      address: veRvrsAddress,
+      name: 'generationRate',
+      params: []
+    },
+    {
+      address: veRvrsAddress,
+      name: 'maxCap',
+      params: []
+    },
+    {
+      address: veRvrsAddress,
+      name: 'withdrawFee',
+      params: []
+    },
+    {
+      address: veRvrsAddress,
+      name: 'withdrawFeeTime',
+      params: []
+    }
+  ];
+  const response = await multicall(veRrvsAbi, calls);
+
+  return response;
 };
