e3c16403fad22259597a853b00f1d5065055d409
0xKillua <zeroxkillua@gmail.com>
2022-04-29 20:20:30 +0400

    vervrs

---
+++ src/pages/veRVRS/card.tsx
@@ -0,0 +1,290 @@
+import BigNumber from 'bignumber.js'
+import React, { useCallback, useState } from 'react'
+import styled, { keyframes } from 'styled-components'
+import { useModal } from '@reverse/uikit'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import { useERC20 } from 'hooks/useContract'
+import { useSousApprove } from 'hooks/useApprove'
+import { useSousStake } from 'hooks/useStake'
+import { useSousUnstake } from 'hooks/useUnstake'
+import { getBalanceNumber } from 'utils/formatBalance'
+import { QuoteToken } from 'config/constants/types'
+import { Pool } from 'state/types'
+import useTokenBalance from 'hooks/useTokenBalance'
+import { getCakeAddress } from 'utils/addressHelpers'
+import Skeleton from 'components/Skeleton/Skeleton'
+import Typography from 'components/layout/typography/typography'
+import { Flex } from 'components/layout/flex'
+import Ripples from 'react-ripples'
+import TypographyBold from 'components/layout/typography/typographyBold'
+import TypographyTitle from 'components/layout/typography/typographyTitle'
+import TitleCard from 'components/layout/cards/TitleCard'
+import ContentCard from 'components/layout/cards/ContentCard'
+import ContentCardAlt from 'components/layout/cards/ContentCardAlt'
+import WithdrawModal from 'components/modals/withdrawModal'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Wrap from 'components/layout/containers/Wrap'
+import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
+import Tippy from '@tippyjs/react'
+import 'tippy.js/dist/tippy.css'
+import LayoutContainer from 'components/layout/containers/LayoutContainer'
+import { usePriceCakeBusd } from '../../state/hooks'
+import StakeModal from '../../components/modals/stakeModal'
+
+interface PoolWithApy extends Pool {
+  apy: BigNumber
+  apr: BigNumber
+}
+
+interface HarvestProps {
+  pool: PoolWithApy
+}
+
+const Card: React.FC<HarvestProps> = ({ pool }) => {
+  const { sousId, stakingTokenName, stakingTokenAddress, apy, userData, pricePerShare, apr } = pool
+
+  // rvrs
+  const rvrsBalance = getBalanceNumber(useTokenBalance(getCakeAddress()))
+  const rvrsBalanceStr = rvrsBalance.toLocaleString('en-us', { maximumFractionDigits: 0, minimumFractionDigits: 0 })
+  const rvrsPrice = usePriceCakeBusd()
+  const stakingTokenContract = useERC20(stakingTokenAddress)
+
+  // func
+  const { account } = useWallet()
+  const { onApprove } = useSousApprove(stakingTokenContract, sousId)
+  const { onStake } = useSousStake(sousId)
+  const { onUnstake } = useSousUnstake(sousId)
+
+  const [requestedApproval, setRequestedApproval] = useState(false)
+  const [pendingTx, setPendingTx] = useState(false)
+
+  const allowance = new BigNumber(userData?.allowance || 0)
+  const stakingTokenBalance = new BigNumber(userData?.stakingTokenBalance || 0)
+  const isOldSyrup = stakingTokenName === QuoteToken.SYRUP
+
+  // staked
+  const staked = new BigNumber(userData?.stakedBalance || 0)
+
+  const stakedNo = getBalanceNumber(staked)
+  const stakedStr = stakedNo.toLocaleString('en-us', { maximumFractionDigits: 2, minimumFractionDigits: 2 })
+  const stakedUsdStr = new BigNumber(stakedNo)
+    .times(rvrsPrice)
+    .toNumber()
+    .toLocaleString('en-us', { maximumFractionDigits: 2, minimumFractionDigits: 2 })
+
+  // misc
+  const accountHasStakedBalance = staked?.toNumber() > 0
+  const needsApproval = !accountHasStakedBalance && !allowance.toNumber()
+
+  // tvl
+  const tvlNo = pool.tvl && pool.tvl.toNumber()
+  const tvlStr = tvlNo.toLocaleString('en-us', { maximumFractionDigits: 0, minimumFractionDigits: 0 })
+
+  // apy
+  const apyNo = apy && apy.toNumber()
+  const apyNull = apyNo < 5
+  const apyStr = apy && apy.toNumber().toLocaleString('en-us', { maximumFractionDigits: 2, minimumFractionDigits: 2 })
+  const monthlyRoiStr = apr
+    .div(12)
+    .toNumber()
+    .toLocaleString('en-us', { maximumFractionDigits: 2, minimumFractionDigits: 2 })
+  const roiYearStr = new BigNumber(apy)
+    .times(stakedNo)
+    .times(0.01)
+    .toNumber()
+    .toLocaleString('en-us', { maximumFractionDigits: 2, minimumFractionDigits: 2 })
+  const roiMonthStr = apr
+    .div(12)
+    .times(stakedNo)
+    .times(0.01)
+    .toNumber()
+    .toLocaleString('en-us', { maximumFractionDigits: 2, minimumFractionDigits: 2 })
+
+  const rewBlock = new BigNumber(0.046).times(1e18).toNumber()
+
+  // approve, withdraw modals
+  const [onPresentWithdraw] = useModal(
+    <WithdrawModal max={staked} onConfirm={onUnstake} tokenName={stakingTokenName} pricePerShare={pricePerShare} />,
+  )
+  const [onPresentDeposit] = useModal(
+    <StakeModal max={stakingTokenBalance} onConfirm={onStake} tokenName={stakingTokenName} />,
+  )
+
+  const notifySuccess = () =>
+    toast.success('Success!', {
+      position: 'top-left',
+      autoClose: 10000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    })
+  const notifyPending = () =>
+    toast.info('Confirm transaction...', {
+      position: 'top-left',
+      autoClose: 10000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    })
+  const Staked = () => new BigNumber(userData?.stakedBalance || 0)
+
+  return (
+    <>
+      {Staked}
+      <Wrap>
+        <LayoutContainer>
+          <TitleCard style={{ marginBottom: '10px' }}>
+            <TypographyTitle>RVRS Staking</TypographyTitle>
+          </TitleCard>
+          <Flex justifyContent="center" marginBottom="10px">
+            <Tippy content="The USD value of all RVRS staked in the protocol">
+              <ContentCard style={{ marginRight: '10px' }}>
+                {pool.apy ? (
+                  <TypographyBold style={{ marginBottom: '5px' }}>${tvlStr}</TypographyBold>
+                ) : (
+                  <Skeleton marginBottom="5px" />
+                )}
+                <Typography>Total Staked</Typography>
+              </ContentCard>
+            </Tippy>
+            <Tippy content="The expected percentage yield (compound interest) of staking RVRS for a year">
+              <ContentCard>
+                {pool.apy ? (
+                  <TypographyBold style={{ marginBottom: '5px' }}>{apyStr}%</TypographyBold>
+                ) : (
+                  <Skeleton marginBottom="5px" />
+                )}
+                <Typography>Annual Yield</Typography>
+              </ContentCard>
+            </Tippy>
+            <Tippy content="The monthly (non-compounded) percentage yield of staking RVRS">
+              <ContentCard style={{ marginLeft: '10px' }}>
+                {pool.apy ? (
+                  <TypographyBold style={{ marginBottom: '5px' }}>{monthlyRoiStr}%</TypographyBold>
+                ) : (
+                  <Skeleton marginBottom="5px" />
+                )}
+                <Typography>Monthly Yield</Typography>
+              </ContentCard>
+            </Tippy>
+          </Flex>
+          <Flex justifyContent="center" marginTop="0px">
+            <Tippy content="Your expected RVRS interest after staking for a year based on current rates">
+              <ContentCardAlt style={{ marginRight: '5px' }}>
+                <TypographyBold style={{ marginBottom: '5px', color: '#6ccca5', fontWeight: '500' }}>
+                  +{roiYearStr} RVRS
+                </TypographyBold>
+                <Typography>Expected Yearly Interest</Typography>
+              </ContentCardAlt>
+            </Tippy>
+            <Tippy content="Your expected RVRS interest after staking for a month based on current rates">
+              <ContentCardAlt>
+                <TypographyBold style={{ marginBottom: '5px', color: '#6ccca5', fontWeight: '500' }}>
+                  +{roiMonthStr} RVRS
+                </TypographyBold>
+                <Typography>Expected Monthly Interest</Typography>
+              </ContentCardAlt>
+            </Tippy>
+          </Flex>
+          {needsApproval ? (
+            <Flex justifyContent="end" style={{ marginTop: '20px' }}>
+              <Ripples>
+                <ActionButton
+                  onClick={async () => {
+                    notifyPending()
+                    setRequestedApproval(true)
+                    await onApprove()
+                    setRequestedApproval(false)
+                    notifySuccess()
+                  }}
+                >
+                  <ToastContainer
+                    position="top-left"
+                    autoClose={10000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                  />
+                  Enable Staking
+                </ActionButton>
+              </Ripples>
+            </Flex>
+          ) : (
+            <Flex alignItems="center" justifyContent="space-between" style={{ marginTop: '20px' }}>
+              <div>
+                <Tippy content={`Your staked balance, currently valued at $${stakedUsdStr}`}>
+                  <ContentCardAlt>
+                    <TypographyBold>
+                      {stakedStr}&nbsp;<Typography>RVRS Staked</Typography>
+                    </TypographyBold>
+                  </ContentCardAlt>
+                </Tippy>
+              </div>
+              {stakedNo > 0 ? (
+                <div style={{ justifyContent: 'space-between' }}>
+                  <Ripples>
+                    <ActionButton style={{ marginRight: '10px' }} onClick={onPresentWithdraw}>
+                      Unstake
+                    </ActionButton>
+                  </Ripples>
+                  <Ripples>
+                    <ActionButton disabled={apyNull} onClick={onPresentDeposit}>
+                      Stake {rvrsBalanceStr} RVRS
+                    </ActionButton>
+                  </Ripples>
+                </div>
+              ) : (
+                <div>
+                  <ActionButton style={{ marginRight: '10px', opacity: '0.3', cursor: 'not-allowed' }} disabled>
+                    Unstake
+                  </ActionButton>
+                  <Ripples>
+                    <ActionButton disabled={apyNull} onClick={onPresentDeposit}>
+                      &nbsp;Stake {rvrsBalanceStr} RVRS
+                    </ActionButton>
+                  </Ripples>
+                </div>
+              )}
+            </Flex>
+          )}
+        </LayoutContainer>
+      </Wrap>
+      <Wrap style={{ marginTop: '20px' }}>
+        <LayoutContainer>
+          <Typography style={{ lineHeight: '1.1' }}>
+            Stakers mint RVRS and gain Governance power over time. This form of staking is being deprecated with the
+            introduction of (ve)RVRS.
+          </Typography>
+        </LayoutContainer>
+      </Wrap>
+    </>
+  )
+}
+
+const ActionButton = styled.button`
+  font-size: 16px;
+  font-weight: 400;
+  background: transparent;
+  color: #eeeeee;
+  border-left: 5px solid #6699a3;
+  padding: 10px;
+  transition: 0.3s ease-in-out;
+  :hover {
+    opacity: 0.5;
+    background: transparent;
+  }
+`
+
+// eslint-disable-next-line import/prefer-default-export
+export { Card }
---
+++ src/pages/veRVRS/staking.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { Route, useRouteMatch } from 'react-router-dom'
+import BigNumber from 'bignumber.js'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import { BLOCKS_PER_YEAR } from 'config'
+import orderBy from 'lodash/orderBy'
+import partition from 'lodash/partition'
+import useBlock from 'hooks/useBlock'
+import { usePools, usePrices, getTotalValueFromQuoteTokens, lookupPrice, useFarmFromPid } from 'state/hooks'
+import Page from 'components/layout/containers/page'
+import { Card as PoolCard } from './card'
+
+export const aprToApy = (apr: number): BigNumber => {
+  const apy = new BigNumber(apr).div(100).div(365).plus(1).pow(365).minus(1).times(100)
+  return apy.isNaN() || !apy.isFinite() ? null : apy
+}
+export const BIG_TEN = new BigNumber(10)
+
+const Staking: React.FC = () => {
+  const { path } = useRouteMatch()
+  const { account } = useWallet()
+  const farm0 = useFarmFromPid(0)
+  let pools = usePools(account)
+  // Filtering for only veRrvrs
+  pools = pools.filter((p) => p.sousId === 1)
+  const prices = usePrices()
+  const block = useBlock()
+  const poolsWithApy = pools.map((pool) => {
+    const quoteTokens = pool.totalStaked
+      ? new BigNumber(pool.totalStaked.toString()).div(2).div(BIG_TEN.pow(18))
+      : new BigNumber(0)
+    const tvl = getTotalValueFromQuoteTokens(quoteTokens, pool.quoteTokenSymbol, prices)
+    const reverseAtlastUserAction = pool.userData
+      ? new BigNumber(pool.userData.reverseAtlastUserAction)
+      : new BigNumber(0)
+    const lastDepositedTime = pool.userData ? new BigNumber(pool.userData.lastDepositedTime) : new BigNumber(0)
+    const lastUserActionTime = pool.userData ? new BigNumber(pool.userData.lastUserActionTime) : new BigNumber(0)
+    const rewardTokenPrice = lookupPrice(pool.tokenName, prices)
+    const totalRewardPricePerYear = rewardTokenPrice
+      .times(farm0.vikingPerBlock)
+      .div(BIG_TEN.pow(18))
+      .times(farm0.poolWeight)
+      .times(BLOCKS_PER_YEAR)
+    const apr = totalRewardPricePerYear.div(tvl).times(100).times(2)
+    const apy = aprToApy(apr)
+    // console.log("TVL", pool.stakingTokenName, tvl && tvl.toNumber(), apy && apy.toNumber())
+    // console.log("APY", pool, tvl && tvl.toNumber())
+    // const totalStakingTokenInPool = stakingTokenPriceInBNB.times(getBalanceNumber(pool.totalStaked))
+    // console.log("price", pool.tokenName, rewardTokenPrice && rewardTokenPrice.toNumber())
+
+    return {
+      ...pool,
+      isFinished: pool.sousId === 0 ? false : pool.isFinished || block > pool.endBlock,
+      apr,
+      apy,
+      tvl,
+      lastDepositedTime,
+      lastUserActionTime,
+      reverseAtlastUserAction,
+    }
+  })
+
+  const [finishedPools, openPools] = partition(poolsWithApy, (pool) => pool.isFinished)
+  const { url, isExact } = useRouteMatch()
+  // console.log(poolsWithApy)
+
+  return (
+    <Page>
+      <Route path={`${path}`}>
+        {orderBy(openPools, ['sortOrder']).map((pool) => (
+          <PoolCard key={pool.sousId} pool={pool} />
+        ))}
+      </Route>
+      <Route path={`${path}/history`}>
+        {orderBy(finishedPools, ['sortOrder']).map((pool) => (
+          <PoolCard key={pool.sousId} pool={pool} />
+        ))}
+      </Route>
+    </Page>
+  )
+}
+
+export default Staking
